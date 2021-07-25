76adaa9df361acf1e65cfdfee160373d7632cc72
Leon <leonlafa@outlook.com>
2021-07-25 20:43:21 +0100

    add html and css

--- src/app/ui/screen/screen.component.ts
+++ src/app/ui/screen/screen.component.ts
@@ -1,15 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-screen',
-  templateUrl: './screen.component.html',
-  styleUrls: ['./screen.component.scss']
+  template: `<div class="ng-switch-screen">
+    <i class="ng-switch-screen__notch"></i>
+    <i class="ng-switch-screen__notch"></i>
+    <div class="ng-switch-screen__reflections"></div>
+    <div class="ng-switch-screen ng-switch-screen--inner"></div>
+  </div>`,
+  styleUrls: ['./screen.component.scss'],
 })
-export class ScreenComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
-}
+export class ScreenComponent {}
