ff44621e1053efdb928fe56262b0dee6fb683916
Leon Lafayette <leonlafa@outlook.com>
2021-07-27 20:36:39 +0100

    feat: create button component (#26)

--- src/app/ui/components/index.ts
+++ src/app/ui/components/index.ts
@@ -1,2 +1,4 @@
-export * from './screen/screen.component';
+export * from './button/button.component';
 export * from './controller/controller.component';
+export * from './joystick/joystick.component';
+export * from './screen/screen.component';
